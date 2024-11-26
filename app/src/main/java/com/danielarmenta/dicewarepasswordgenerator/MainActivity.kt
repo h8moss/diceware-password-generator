fe76550f0e047d15731895b347a3b6845173d92a
h8m0ss <danarmenta32@gmail.com>
2024-11-26 08:38:53 -0600

    Removed unnesesary code

--- app/src/main/java/com/danielarmenta/dicewarepasswordgenerator/MainActivity.kt
+++ app/src/main/java/com/danielarmenta/dicewarepasswordgenerator/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.danielarmenta.dicewarepasswordgenerator
 
-import android.content.Context
-import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,7 +8,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -21,26 +18,21 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.danielarmenta.dicewarepasswordgenerator.data.WordManager
 import com.danielarmenta.dicewarepasswordgenerator.ui.theme.DicewarePasswordGeneratorTheme
-import kotlin.time.Duration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,21 +47,21 @@ class MainActivity : ComponentActivity() {
 }
 
 fun entropyOf(count: Int): Double {
-    return 12.92 * count;
+    return 12.92 * count
 }
 
 fun intToDices(i: Int): String {
-    val nums = arrayListOf<Int>();
-    var myI = i;
+    val nums = arrayListOf<Int>()
+    var myI = i
     while (myI > 0) {
-        val mod = myI % 6;
-        print(mod);
-        nums.add(mod+1);
-        myI -= mod;
-        myI /= 6;
+        val mod = myI % 6
+        print(mod)
+        nums.add(mod+1)
+        myI -= mod
+        myI /= 6
     }
 
-    return nums.reversed().joinToString("");
+    return nums.reversed().joinToString("")
 }
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -82,7 +74,7 @@ fun WordsComponent(words: Array<Int>, modifier: Modifier = Modifier) {
         horizontalArrangement = Arrangement.Center
     ) {
         words.forEach { i ->
-            val word = WordManager.words[i];
+            val word = WordManager.words[i]
             Column(
                 modifier = Modifier.padding(8.dp),
                 verticalArrangement = Arrangement.Center
@@ -90,10 +82,10 @@ fun WordsComponent(words: Array<Int>, modifier: Modifier = Modifier) {
                 Text(intToDices(i),
                     fontSize = 12.sp,
                     color = Color(0xFF404040),
-                    );
+                    )
                 Text("$word ",
                     fontSize = 30.sp,
-                    );
+                    )
             }
         }
     }
@@ -102,9 +94,9 @@ fun WordsComponent(words: Array<Int>, modifier: Modifier = Modifier) {
 @Composable
 fun App(modifier: Modifier = Modifier) {
     var wordIndexes by remember {
-        mutableStateOf(arrayOf<Int>());
+        mutableStateOf(arrayOf<Int>())
     }
-    val entropy = entropyOf(wordIndexes.size);
+    val entropy = entropyOf(wordIndexes.size)
 
     Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
         Column(
@@ -128,7 +120,7 @@ fun App(modifier: Modifier = Modifier) {
                 Text(text = "Add word")
             }
             Button(onClick = {
-                val s = (wordIndexes.size-1);
+                val s = (wordIndexes.size-1)
                 wordIndexes = wordIndexes.slice(0..<s).toTypedArray()
             }) {
                 Icon(
@@ -139,7 +131,7 @@ fun App(modifier: Modifier = Modifier) {
                 Text(text = "Remove word")
             }
             Button(onClick = {
-                wordIndexes = arrayOf();
+                wordIndexes = arrayOf()
             }) {
                 Icon(
                     Icons.Filled.Refresh,
